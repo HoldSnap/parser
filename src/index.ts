4bc0de23dfc1ceaf1b98ee3d27a25b2c68d0fe78
Stepanov Mikhail <volodya-stepanov-68@gmail.ru>
2025-04-29 21:24:08 +0300

    build: create docker

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log("Привет из TypeScript!");
+import express from "express";
+
+const app = express();
+const PORT = 8080;
+
+app.get("/", (_req, res) => {
+  res.send("Привет из TypeScript!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Сервер запущен на порту ${PORT}`);
+});
