cf95ee01f787e90e90e7a43a93f1764a0ef0c5e3
Stepanov Mikhail <volodya-stepanov-68@gmail.ru>
2025-04-29 21:48:02 +0300

    refactor: eslint styles

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,33 @@
-import js from "@eslint/js";
-import globals from "globals";
-import tseslint from "typescript-eslint";
+import stylisticTs from "@stylistic/eslint-plugin-ts";
+import parserTs from "@typescript-eslint/parser";
 import { defineConfig } from "eslint/config";
 
-
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
-  { files: ["**/*.{js,mjs,cjs,ts}"], languageOptions: { globals: globals.browser } },
-  tseslint.configs.recommended,
+  { ignores: ["dist/**", "node_modules/**"] },
+  {
+    files: ["**/*.{ts,tsx}"],
+    languageOptions: {
+      parser: parserTs,
+      sourceType: "module",
+    },
+    plugins: {
+      "@stylistic/ts": stylisticTs,
+    },
+    rules: {
+      "no-multiple-empty-lines": ["error", { max: 1, maxEOF: 0 }],
+
+      "@stylistic/ts/padding-line-between-statements": [
+        "error",
+        { blankLine: "always", prev: "*", next: "return" },
+        { blankLine: "always", prev: ["block", "block-like"], next: "*" },
+        { blankLine: "never", prev: "directive", next: "*" },
+      ],
+
+      "@stylistic/ts/indent": ["error", 2],
+      "@stylistic/ts/space-infix-ops": ["error"],
+      "@stylistic/ts/keyword-spacing": ["error", { before: true, after: true }],
+      "@stylistic/ts/space-before-function-paren": ["error", "never"],
+      "@stylistic/ts/semi-spacing": ["error", { before: false, after: true }],
+    },
+  },
 ]);
