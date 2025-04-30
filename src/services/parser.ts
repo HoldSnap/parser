b07a654f50e51159a3477b78747fc2a4993c4ca4
Stepanov Mikhail <volodya-stepanov-68@gmail.ru>
2025-04-30 20:49:50 +0300

    fix: update parser

--- src/services/parser.ts
+++ src/services/parser.ts
@@ -9,15 +9,17 @@ export async function fetchAndExtractWords(
   const { data } = await axios.get<string>(url);
   const $ = cheerio.load(data);
 
-  const htmlWithSpaces = $("body").html()?.replace(/></g, "> <") || "";
-  let text = cheerio.load(htmlWithSpaces).text();
+  $("script, style, noscript").remove();
 
-  text = text.replace(/[_-]+/g, " ");
-  text = text.replace(/[^A-Za-zА-ЯЁа-яё0-9]+/g, " ").trim();
+  let text = $("body").text();
 
-  text = text.replace(/([a-zа-яё])([A-ZА-ЯЁ])/g, "$1 $2");
-  text = text.replace(/([0-9])([A-Za-zА-ЯЁа-яё])/g, "$1 $2");
-  text = text.replace(/([A-Za-zА-ЯЁа-яё])([0-9])/g, "$1 $2");
+  text = text
+    .replace(/[_-]+/g, " ")
+    .replace(/[^A-Za-zА-ЯЁа-яё0-9]+/g, " ")
+    .trim()
+    .replace(/([a-zа-яё])([A-ZА-ЯЁ])/g, "$1 $2")
+    .replace(/([0-9])([A-Za-zА-ЯЁа-яё])/g, "$1 $2")
+    .replace(/([A-Za-zА-ЯЁа-яё])([0-9])/g, "$1 $2");
 
   return topLongestWords(text, limit);
 }
